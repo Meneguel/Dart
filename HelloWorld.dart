5c6e3b4270def2acf7ace3d7e4b207bf7e1e96f8
Rodrigo Meneguel <r_menega@hotmail.com>
2021-08-04 17:00:09 -0300

    Hello World

--- HelloWorld.dart
+++ HelloWorld.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 main() {
+  print("Qual a sua Idade?");
 
-  var idade = 50;
-
+  var input = stdin.readLineSync();
+  var idade = int.parse(input!);
   if (idade >= 18) {
     print("Maior");
   } else {
